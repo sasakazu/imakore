3341852ddba9dfbc1f1188c6ce419b10772d3a6c
sasakazu <kazuya.sasakura@gmail.com>
2021-08-18 18:22:23 +0900

    Initial Commit

---
+++ imakore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  imakore
+//
+//  Created by 笹倉一也 on 2021/08/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
